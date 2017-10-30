c7c371a01cc01c112f40806f5d5cdd7df0787186
Vivek S. Buzruk <vivek_buzruk@yahoo.com>
2017-10-30 15:41:53 +0530

    Need few functions

--- reportng/src/java/resources/org/uncommons/reportng/templates/html/reportng.js
+++ reportng/src/java/resources/org/uncommons/reportng/templates/html/reportng.js
@@ -12,3 +12,31 @@ function toggle(toggleId)
     var toggle = document.getElementById ? document.getElementById(toggleId) : document.all[toggleId];
     toggle.textContent = toggle.innerHTML == '\u25b6' ? '\u25bc' : '\u25b6';
 }
+
+function imageExists(imageUrl, url1, url2, callback) {
+	var img = new Image();
+
+	img.onload = function() { callback(true, url1); };
+	img.onerror = function() { callback(false, url2); };
+	img.src = imageUrl;
+}
+function getImageUrl(imageUrl, url1, url2) {
+	imageExists(imageUrl, url1, url2, function(exists, url) {
+		window.open(url,"_blank","","");
+		return true;
+	});
+	return true;
+}
+function getImageUrl2(imageUrl, url1, url2) {
+	var image = new Image();
+	image.src = imageUrl;
+	if (!image.complete) {
+		window.open(url2,"_blank","","");
+		return true;
+	} else if (image.height === 0) {
+		window.open(url2,"_blank","","");
+		return true;
+	}
+	window.open(url1,"_blank","","");
+	return true;
+}
